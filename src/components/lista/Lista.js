8a7b887adafa7bfa913c46da47ec1f6910b79b31
Felipe Pinheiro <68909633+felipe207@users.noreply.github.com>
2025-01-07 13:09:01 -0300

    dinamizando componente

--- src/components/lista/Lista.js
+++ src/components/lista/Lista.js
@@ -1,40 +1,63 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 import './Lista.css';
 
 const Lista = () => {
-
-    let musicas = [
-        {
-            'titulo': 'O Mineiro e o Italiano',
-            'visualizacoes': 5200000,
-            'youtube_id': 's9kVG2ZaTS4',
-            'thumb': 'https://img.youtube.com/vi/s9kVG2ZaTS4/hqdefault.jpg'
-        },
-        {
-            'titulo': 'Pagode em Brasília',
-            'visualizacoes': 5000000,
-            'youtube_id': 'lpGGNA6_920',
-            'thumb': 'https://img.youtube.com/vi/lpGGNA6_920/hqdefault.jpg'
-        },
-        {
-            'titulo': 'Rio de Lágrimas',
-            'visualizacoes': 153000,
-            'youtube_id': 'FxXXvPL3JIg',
-            'thumb': 'https://img.youtube.com/vi/FxXXvPL3JIg/hqdefault.jpg'
-        },
-        {
-            'titulo': 'Tristeza do Jeca',
-            'visualizacoes': 154000,
-            'youtube_id': 'tRQ2PWlCcZk',
-            'thumb': 'https://img.youtube.com/vi/tRQ2PWlCcZk/hqdefault.jpg'
-        },
-        {
-            'titulo': 'Terra roxa',
-            'visualizacoes': 3300000,
-            'youtube_id': '4Nb89GFu2g4',
-            'thumb': 'https://img.youtube.com/vi/4Nb89GFu2g4/hqdefault.jpg'
+    // pega os valores da api loccalhost:8000/api/musicas
+    const [musicas, setMusicas] = useState([]);
+    const [error, setError] = useState(null);
+  
+    useEffect(() => {
+      const fetchMusicas = async () => {
+        try {
+          const response = await axios.get("http://localhost:8000/api/musicas");
+          if (response.data.results) {
+            setMusicas(response.data.results);
+          } else {
+            setError("Nenhum resultado encontrado.");
+          }
+        } catch (err) {
+          setError(err.response?.data?.message || "Erro ao buscar músicas.");
         }
-    ];
+      };
+  
+      fetchMusicas();
+    }, []);
+
+
+
+    // let musicas = [
+    //     {
+    //         'titulo': 'O Mineiro e o Italiano',
+    //         'visualizacoes': 5200000,
+    //         'youtube_id': 's9kVG2ZaTS4',
+    //         'thumb': 'https://img.youtube.com/vi/s9kVG2ZaTS4/hqdefault.jpg'
+    //     },
+    //     {
+    //         'titulo': 'Pagode em Brasília',
+    //         'visualizacoes': 5000000,
+    //         'youtube_id': 'lpGGNA6_920',
+    //         'thumb': 'https://img.youtube.com/vi/lpGGNA6_920/hqdefault.jpg'
+    //     },
+    //     {
+    //         'titulo': 'Rio de Lágrimas',
+    //         'visualizacoes': 153000,
+    //         'youtube_id': 'FxXXvPL3JIg',
+    //         'thumb': 'https://img.youtube.com/vi/FxXXvPL3JIg/hqdefault.jpg'
+    //     },
+    //     {
+    //         'titulo': 'Tristeza do Jeca',
+    //         'visualizacoes': 154000,
+    //         'youtube_id': 'tRQ2PWlCcZk',
+    //         'thumb': 'https://img.youtube.com/vi/tRQ2PWlCcZk/hqdefault.jpg'
+    //     },
+    //     {
+    //         'titulo': 'Terra roxa',
+    //         'visualizacoes': 3300000,
+    //         'youtube_id': '4Nb89GFu2g4',
+    //         'thumb': 'https://img.youtube.com/vi/4Nb89GFu2g4/hqdefault.jpg'
+    //     }
+    // ];
 
     function formatViews(views) {
         if (views >= 1000000) {
